537eb7461a879e873eccb6838e8a0f26fc1e54de
Dew <summerdewyes@gmail.com>
2021-10-16 18:43:55 +0900

    Initial commit

---
+++ app/src/main/java/com/summerdewyes/mvvm_check_bang/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.summerdewyes.mvvm_check_bang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
